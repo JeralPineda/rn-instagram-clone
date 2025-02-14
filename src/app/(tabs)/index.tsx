c2f805b3fa41a2f9e0fe02679b246230e4d1eac9
Jeral Pineda <jeral.cris@gmail.com>
2025-02-13 22:45:22 -0600

    fix: rename pages

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Page() {
+export default function Feed() {
   return (
     <View style={styles.container}>
       <Text>Tab Page</Text>
